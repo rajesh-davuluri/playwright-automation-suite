c901f9c71749905f56e3d4174a8e3e0f3f0fcc6c
rajesh-davuluri <rajeshdavuluri@hotmail.com>
2026-06-25 11:08:33 -0400

    class file has been added

--- TypeScript Practice/Lessons/Bharath/Arrays/Strings/strExample3.ts
+++ TypeScript Practice/Lessons/Bharath/Arrays/Strings/strExample3.ts
@@ -1,2 +1,3 @@
 let str5:string=" Hello Husband :)"
+console.log(str5.trim())
 console.log(str5)
---
+++ TypeScript Practice/Lessons/Bharath/Classes/exampleClass.ts
@@ -0,0 +1,27 @@
+//Class example
+
+class Employee
+{ //starting body of the class
+    //property
+    eid:number;
+    ename:string;
+    eemail:string;
+
+    constructor(id:number, name:string, email:string)
+    {
+        this.eid=id;
+        this.ename=name;
+        this.eemail= email;
+    }
+
+
+
+}// ending/close body of the class
+
+let empdetails = new Employee(202, "rajesh", "raj@test.com");
+//Approach 1
+console.log(empdetails.eid)
+console.log(empdetails.ename)
+//Approach 2
+console.log(empdetails['eid'])
+
